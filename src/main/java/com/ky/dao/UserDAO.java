3acf7f15c6a551678946838a5b78df4de49eed4b
KrishnaYamphu <krishnayamphu@gmail.com>
2022-11-27 14:37:15 +0545

    user login

--- src/main/java/com/ky/dao/UserDAO.java
+++ src/main/java/com/ky/dao/UserDAO.java
@@ -5,6 +5,7 @@ import com.ky.models.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
@@ -22,5 +23,24 @@ public class UserDAO {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    public static boolean auth(String username, String password){
+        boolean staus=false;
+        String sql = "SELECT username,password FROM users WHERE username=? AND password=?";
+        try{
+            Connection con = ConnectDB.connect();
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, username);
+            ps.setString(2, password);
+            ResultSet rs=ps.executeQuery();
+            while (rs.next()){
+                staus=true;
+            }
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return staus;
     }
 }
