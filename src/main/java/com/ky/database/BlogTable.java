370b4968b88feb9522b8fe8edb1d396e6517f039
KrishnaYamphu <krishnayamphu@gmail.com>
2022-11-28 14:22:38 +0545

    blog table

---
+++ src/main/java/com/ky/database/BlogTable.java
@@ -0,0 +1,45 @@
+package com.ky.database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+
+public class BlogTable {
+    static Connection cn;
+    public static void create(){
+        try {
+            cn=ConnectDB.connect();
+            String sql="CREATE TABLE blog (id int PRIMARY KEY AUTO_INCREMENT, title VARCHAR(256) NOT NULL,content text,user_id int NOT NULL, picture VARCHAR(256), created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, updated_at TIMESTAMP, UNIQUE(title))";
+            PreparedStatement ps=cn.prepareStatement(sql);
+            ps.executeUpdate();
+            System.out.println("Blog table created");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                cn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void drop(){
+        try {
+            cn=ConnectDB.connect();
+            String sql="DROP TABLE blog";
+            PreparedStatement ps=cn.prepareStatement(sql);
+            ps.executeUpdate();
+            System.out.println("Blog table deleted");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                cn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
